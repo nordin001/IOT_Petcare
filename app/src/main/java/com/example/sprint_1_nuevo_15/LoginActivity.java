0f8157b76b67689da9780473a19ffe5cd0c40a00
MimiVladeva <mimivladeva@gmail.com>
2023-10-20 22:25:52 +0200

    commit mimi prueba menu fallo pero me tengo que ir a comer

--- app/src/main/java/com/example/sprint_1_nuevo_15/LoginActivity.java
+++ app/src/main/java/com/example/sprint_1_nuevo_15/LoginActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -64,20 +65,36 @@ public class LoginActivity extends AppCompatActivity {
         return username.equals("your_valid_username") && password.equals("your_valid_password");
     }
 
-
+//menu
     @Override public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true; /** true -> el menú ya está visible */
     }
 
+
+    public static class AcercaDeActivity extends AppCompatActivity {
+        @Override public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.acercade);
+
+        }
+
+
+    }
+    public void lanzarAcercaDe(View view){
+        Intent i = new Intent(this, AcercaDeActivity.class);
+        startActivity(i);
+    }
+
+
     @Override public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.acercaDe) {
+            lanzarAcercaDe(null);
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
-
 
 }
 
